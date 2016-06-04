5a9858afe90729bd280be35f2fd78dcb14689bec
MarcStan <marcstan@live.com>
2016-06-04 12:20:10 +0200

    added input to contentscene which of course doesn't work yet, see https://github.com/MarcStan/MonoGame.Framework.WpfInterop/issues/1

--- WpfTest/ContentScene.cs
+++ WpfTest/ContentScene.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using MonoGame.Framework.WpfInterop;
-using System;
 
 namespace WpfTest
 {
@@ -13,6 +13,12 @@ namespace WpfTest
 		private Texture2D _texture;
 		private IGraphicsDeviceService _graphicsDeviceManager;
 		private SpriteBatch _spriteBatch;
+
+		private KeyboardState _keyboard;
+		private MouseState _mouse;
+		private int posX = 100, posY = 100;
+		private float _rotation;
+		private bool _mouseDown;
 
 		public override void Initialize()
 		{
@@ -25,11 +31,15 @@ namespace WpfTest
 			_texture = _content.Load<Texture2D>("hello");
 
 			_spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			_keyboard = Keyboard.GetState();
 		}
 
 		public override void Render(GameTime time)
 		{
-			GraphicsDevice.Clear(Color.Black);
+			Update(time);
+
+			GraphicsDevice.Clear(_mouseDown ? Color.CornflowerBlue : Color.Black);
 
 			// since we share the GraphicsDevice with all hosts, we need to save and reset the states
 			// this has to be done because spriteBatch internally sets states and doesn't reset themselves, fucking over any 3D rendering (which happens in the DemoScene)
@@ -40,7 +50,7 @@ namespace WpfTest
 			var sampler = GraphicsDevice.SamplerStates[0];
 
 			_spriteBatch.Begin();
-			_spriteBatch.Draw(_texture, new Vector2(100, 100), Color.White);
+			_spriteBatch.Draw(_texture, new Rectangle(posX, posY, 100, 20), null, Color.White, _rotation, new Vector2(_texture.Width, _texture.Height) / 2f, SpriteEffects.None, 0);
 			_spriteBatch.End();
 
 			GraphicsDevice.BlendState = blend;
@@ -51,5 +61,20 @@ namespace WpfTest
 			base.Render(time);
 		}
 
+		private void Update(GameTime time)
+		{
+			_keyboard = Keyboard.GetState();
+			_mouse = Mouse.GetState();
+
+			if (_keyboard.IsKeyDown(Keys.Right))
+			{
+				_rotation += 0.05f;
+			}
+			if (_keyboard.IsKeyDown(Keys.Left))
+			{
+				_rotation -= 0.05f;
+			}
+			_mouseDown = _mouse.LeftButton == ButtonState.Pressed;
+		}
 	}
 }
